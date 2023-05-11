2135d4eecf6142a0e8426e8dfac6952d6abbcd7f
Antonio Sabino <aslimajr@outlook.com>
2023-05-11 20:36:48 -0300

    feat(app): adicionar interceptor global ConflictInterceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +14,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
 
   await app.listen(process.env.PORT);
 }
