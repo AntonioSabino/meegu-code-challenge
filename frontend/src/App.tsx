80a222400cd8b5d6a1dc3ea185f787ab5fad3376
Antonio Sabino <aslimajr@outlook.com>
2023-05-12 14:26:28 -0300

    feat: adicionar defaultTheme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <></>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>App</h1>
+    </ThemeProvider>
+  )
 }
 
 export default App
